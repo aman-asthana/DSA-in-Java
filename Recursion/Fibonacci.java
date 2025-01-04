0fd29214ce461fe6137d75a1e8945b3bae02a12e
Aman Asthana <amanasthana431@gmail.com>
2025-01-05 00:17:23 +0530

    Fibonacci Series

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -1,17 +1,14 @@
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int n = 5;
-        for (int i = 1; i <=n; i++) {
-            System.out.print(fibo(i));
-        }
+        int n = 9;
         
+            System.out.print(fibo(n));
     }
     static int fibo(int n){
-        if(n<=1){
+        if((n-2<0)){
             return n;
         }
-        
         return fibo(n-1) + fibo(n-2);
         
     }        
