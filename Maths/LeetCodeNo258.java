125a433f08f2997249dd8b12f4d5baa47ff00677
Aman Asthana <amanasthana431@gmail.com>
2025-01-15 00:40:23 +0530

    Add Digits

---
+++ Maths/LeetCodeNo258.java
@@ -0,0 +1,15 @@
+public class LeetCodeNo258 {
+    public static void main(String[] args) {
+        int num = 38;
+        System.out.println(addDigits(num));
+    }
+    static int addDigits(int num) {
+        int sum = 0;
+        while(num > 0) {
+            int end = num % 10;
+            sum += end;
+            num = num / 10;
+        }
+        return sum >= 10 ? addDigits(sum) : sum;
+    }
+}
