3177d27d862e939e52570ded0b08a95914b2980f
Aman Asthana <amanasthana431@gmail.com>
2025-01-27 00:51:26 +0530

    A Number After a Double ReversalA Number After a Double ReversalA Number After a Double Reversal

---
+++ Maths/LeetCodeNo2119.java
@@ -0,0 +1,14 @@
+public class LeetCodeNo2119 {
+    public static void main(String[] args) {
+        int num = 1800;
+        System.out.println(isSameAfterReversals(num));
+    }
+    static boolean isSameAfterReversals(int num) {
+        if(num/10==0){
+            return true;
+        }else if(num%10==0){
+            return false;
+        }
+        return true;
+    }
+}
