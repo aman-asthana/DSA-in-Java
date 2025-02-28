26cfb43127fe834f6029d9bb6d19ab14132f56ee
Aman Asthana <amanasthana431@gmail.com>
2025-03-01 00:03:47 +0530

    Rotate image (Rotate matrix 90 degree)

---
+++ Arrays/LeetCodeNo48.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+public class LeetCodeNo48{
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+        rotate(matrix);
+        System.out.println(Arrays.toString(matrix[0]));
+        System.out.println(Arrays.toString(matrix[1]));
+        System.out.println(Arrays.toString(matrix[2]));
+    }
+    static void rotate(int[][] matrix) {
+        for(int i = 0; i<matrix.length; i++){
+            for(int j = i; j<matrix[0].length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0; i< matrix.length; i++){
+            int start = 0;
+            int end = matrix[i].length-1;
+            while(start<=end){
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++;
+                end--;
+            }
+        }
+    }
+}
